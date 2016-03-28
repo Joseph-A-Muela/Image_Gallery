2eba6e218ac70df66afbb48db9c779e242bcc1e0
Joseph-A-Muela <josephamuela@gmail.com>
2016-03-27 22:05:44 -0500

    Cant get the caption to display.

---
+++ scripts/main.js
@@ -0,0 +1,65 @@
+var parentContainer = $('.parentContainer');
+var icon = $('.glyphicon-plus-sign');
+var addBtn = $('.addBtn');
+var cancelBtn = $('.cancelBtn');
+var urlImage = $('.urlImage');
+var urlCaption = $('urlCaption');
+var photoReal = $('.photoReal');
+var imgContainer = $('.imgContainer');
+var deleteAllBtn = $('.deleteAllBtn');
+var url = 'http://small-tiyfe.herokuapp.com/collections/joseph-images';
+
+
+parentContainer.hide();
+    icon.click(function(e) {
+    e.preventDefault();
+    parentContainer.slideDown('slow');
+
+    $.get(
+        url,
+        function(pullData){
+	            pullData.forEach(function(pullData) {
+				var newPhotoReal = '<img src="' + pullData.url + '"><div>' + pullData.caption + '</div>';
+				photoReal.append(newPhotoReal);
+            });
+        }
+    );
+
+    imgContainer.show();
+
+});
+
+//////////////////////////////////////////////////////////////////
+
+cancelBtn.click(function(e) {
+    e.preventDefault();
+    urlImage = urlImage.val('');
+    urlCaption = urlCaption.val('');
+    parentContainer.slideUp('slow');
+});
+
+//////////////////////////////////////////////////////////////////
+
+    addBtn.click(function(e) {
+    e.preventDefault();
+    var newUrlImage = urlImage.val();
+    var newUrlCaption = urlCaption.val();
+
+    $.post(
+        url,
+        {
+        	url: newUrlImage,
+        	caption: newUrlCaption
+        },
+        function(data) {
+            photoReal.append('<img src="'+data.url+'">' + '<div>'+data.caption+'</div>');
+            photoReal.show();
+        },
+        'json'
+    )
+});
+
+
+
+
+
